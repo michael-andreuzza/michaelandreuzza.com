81b82d415147944835f9e33c8b8043b2501fb1e6
michael-andreuzza <michaelandreuzzavega@gmail.com>
2021-10-23 12:13:55 +0300

    blue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,12 @@
 const colors = require('tailwindcss/colors');
 module.exports = {
-
   mode: 'jit',
- purge: ['./src/**/*.html'],
+  dark: 'media',
+  purge: ['./src/**/*.html'],
   theme: {
     extend: {
       backgroundImage: (theme) => ({
         wickedtemplates: "url('../images/wickedtemplates.svg')",
-
       }),
       height: (theme) => ({
         "screen/2": "50vh",
@@ -15,41 +14,41 @@ module.exports = {
         "screen/4": "calc(100vh / 4)",
         "screen/5": "calc(100vh / 5)",
       }),
-        boxShadow: {
+      boxShadow: {
         DEFAULT: '0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24)',
         thick: 'rgba(0, 0, 0, 0.3) 0px 13px 40px, rgba(0, 0, 0, 0.2) 0px 4px 4px;',
       },
-       borderWidth: {
+      borderWidth: {
         DEFAULT: '0.5px',
         '1': '1px',
-
       },
-
       colors: {
-
-
-
         gossip: '#9cd976',
 
-
-        lilac : '#ededed',
-        aluminium : '#868787',
-        mercury : '#818181',
-        jumbo : '#858585',
-        monsoon: '#787878',
-        chicago: '#5F5F5F',
-        charcoal : '#454545',
-        midnight: '#2C2C2C',
-        darko: '#222425',
-        jungle: '#1b1d1d',
-        bunker: '#111213',
-        onyx: '#171717',
+        water: '#dcdefa',
+        perano: '#b8bef5',
+        slate: '#636eea',
+        royal: '#3f4ee4',
+        palatino: '#2f3ee3',
+        leblue: '#1d2ee0',
 
 
-
+        
+        ebony: '#050722',
+        tiara: '#bfd3d6',
+        smoke: "#f0f0f0",
+        santa: "#a0a1b2",
+        comet: "#5b617a",
+        bay: "#50566c",
+        river: "#464a5d",
+        bright: "#3c3e4e",
+        tuna: "#313340",
+        haiti: "#2c2c35",
+        cinder: "#252731",
+        pearl: "#1e2028",
+        mirage: "#1a1c23",
       },
       fontFamily: {
-
         sans: [
           'Inter',
           'system-ui',
@@ -63,12 +62,13 @@ module.exports = {
           'sans-serif',
           '"Apple Color Emoji"',
           '"Segoe UI Emoji"',
-          '"Segoe UI Symbol"',] // Ensure fonts with spaces have " " surrounding it.
+          '"Segoe UI Symbol"',
+        ] // Ensure fonts with spaces have " " surrounding it.
       },
     },
   },
   variants: {
-    extend: {    },
+    extend: {},
   },
   plugins: [
     require('tailwindcss-scroll-snap'),

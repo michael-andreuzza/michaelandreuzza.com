d9e2ab8aa6460a5335c65a7f3bc8d6e74559d4b7
Michael Andreuzza <Michaelandreuzzavega@gmail.com>
2022-05-28 20:00:17 +0300

    svg gradients

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ module.exports = {
   content: ['./src/**/*.html'],
   theme: {
     extend: {
+      
       colors: {
         black: "#1e2028",
       },
@@ -20,8 +21,23 @@ module.exports = {
           '"Apple Color Emoji"',
           '"Segoe UI Emoji"',
           '"Segoe UI Symbol"',
+        ],
+        display: [
+          '"Ceqabro"',
+          'system-ui',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          '"Noto Sans"',
+          'sans-serif',
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
         ], // Ensure fonts with spaces have " " surrounding it.
-        
+
       },
     },
   },

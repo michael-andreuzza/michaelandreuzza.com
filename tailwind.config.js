09ead9a57dd512aa5e46bb70678da3cd642647ab
Michael Andreuzza <Michaelandreuzzavega@gmail.com>
2022-06-23 00:56:15 +0200

    cleaner

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,10 @@ module.exports = {
 
       colors: {
         black: "#0d0d0d",
-        white: "#f1f0e4",
+
       },
       fontFamily: {
-        
+
         display: [
           '"NeueHaasDisplayBlack"',
           "system-ui",
@@ -24,21 +24,7 @@ module.exports = {
           '"Segoe UI Emoji"',
           '"Segoe UI Symbol"',
         ],
-        ds: [
-          '"NeueHaasGroteskDisp Pro Md"',
-          "system-ui",
-          "-apple-system",
-          "BlinkMacSystemFont",
-          '"Segoe UI"',
-          "Roboto",
-          '"Helvetica Neue"',
-          "Arial",
-          '"Noto Sans"',
-          "sans-serif",
-          '"Apple Color Emoji"',
-          '"Segoe UI Emoji"',
-          '"Segoe UI Symbol"',
-        ],
+        
       },
     },
   },
